296fcf5f4c94d67b3cf4b28e6c26bd816c8ba8fa
sohail khan <sohail@bluell.se>
2026-01-19 16:20:03 +0500

    fixed data token issue

--- src/utils/jwtService.js
+++ src/utils/jwtService.js
@@ -4,7 +4,7 @@ const config = require("./config");
 const signToken = (
   payload,
   secret = config.JWT_TOKEN_SECRET,
-  expiresIn = "1h",
+  expiresIn = "7d",
 ) => {
   return jwt.sign(payload, secret, { expiresIn });
 };
