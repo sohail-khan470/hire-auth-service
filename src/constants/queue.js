36ec81d52ceba3c6bbbcb2a0e388d10b52fac23b
sohail khan <sohail@bluell.se>
2026-01-21 17:55:21 +0500

    fixed something

--- src/constants/queue.js
+++ src/constants/queue.js
@@ -1,4 +1,4 @@
 module.exports = {
-  USER_CREATED_QUEUE: "auth.user.created.queue",
-  USER_VERIFIED_QUEUE: "auth.user.verified.queue",
+  USER_CREATED_QUEUE: "auth-service.user.created.queue",
+  USER_VERIFIED_QUEUE: "auth-service.user.verified.queue",
 };
